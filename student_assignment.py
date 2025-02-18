17f3c42fa984ad34106ca14490a8e3a1ed0e0140
Eric6_Chen <eric6_chen@asus.com>
2025-02-18 15:33:58 +0800

    update hw01 to try the output testing

--- student_assignment.py
+++ student_assignment.py
@@ -1,13 +1,25 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import (CharacterTextSplitter,
-                                      RecursiveCharacterTextSplitter)
+from langchain_community.document_loaders import PyPDFLoader, PyMuPDFLoader
+from langchain_text_splitters import (
+    CharacterTextSplitter,
+    RecursiveCharacterTextSplitter,
+)
 
-q1_pdf = "OpenSourceLicenses.pdf"
-q2_pdf = "勞動基準法.pdf"
+q1_pdf = r"OpenSourceLicenses.pdf"
+q2_pdf = r"勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    loader = PyPDFLoader(q1_pdf)
+    document = loader.load()
+    last = document[-1]
+    return str(last.page_content) + str(last.metadata)
+
 
 def hw02_2(q2_pdf):
     pass
+
+
+if __name__ == "__main__":
+    print("============main start============")
+    print("result of hw02_1:\n" + hw02_1(q1_pdf))
+    print("============main end==============")
